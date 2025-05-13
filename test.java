f95073b5384437499c236b42df1bfac892021048
Lalit Kumar <45649399+hellolalit@users.noreply.github.com>
2025-05-13 14:05:29 +0530

    Test files
    Test files

---
+++ test.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class test 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        float a,b,sum;
+        System.out.println("Please Enter the value of A = ");
+        a = sc.nextFloat();
+        System.out.println("Please Enter the value of B = ");
+        b = sc.nextFloat();
+        sum = a + b;
+        System.out.println("The Sum of A and B is =" + sum);
+    }
+}

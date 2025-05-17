ff7be62f20777e22ea52b0b3581b06dcfaa5f010
Lalit Kumar <45649399+hellolalit@users.noreply.github.com>
2025-05-17 18:56:27 +0530

    Created self project in html table 01-Invoice/bill for Rashmi's Company

---
+++ doubt.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class doubt 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scr = new Scanner(System.in);
+        System.out.println("The Doubts are listed below :- ");
+        System.out.println("1.Why I have to use &nbsp; to create space in colorm. Any industry standard way available?");
+    }
+}

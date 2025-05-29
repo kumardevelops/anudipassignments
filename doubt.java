0e4594006be604e8a3cc13794b21c1503be0debe
Mr. Kumar <lkt929129@gmail.com>
2025-05-29 17:40:56 +0530

    tables almost done, form created but now working

--- doubt.java
+++ doubt.java
@@ -1,10 +1,9 @@
-import java.util.*;
-public class doubt 
-{
-    public static void main(String[] args) 
-    {
-        Scanner scr = new Scanner(System.in);
+
+public class doubt {
+
+    public static void main(String[] args) {
         System.out.println("The Doubts are listed below :- ");
-        System.out.println("1.Why I have to use &nbsp; to create space in colorm. Any industry standard way available?");
+        System.out.println("1.Why I have to use &nbsp; to create space in column. Any industry standard way available?\n");
+        System.out.println("2. /tebles/01invoice- line 82 = whithout colspan/rowspan how can eat the space and fix the alignment?");
     }
 }
